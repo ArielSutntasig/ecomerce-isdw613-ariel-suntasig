1aeaae2cf4aff71bf6f363be56d01d19a4888918
ArielSutntasig <denis.suntasig@epn.edu.ec>
2024-01-06 17:10:05 -0500

    JQUERY and  AJAX

--- js/test-script.js
+++ js/test-script.js
@@ -4,3 +4,14 @@ function suma(a, b) {
 }
 
 console.log('Hola mundo: ' + suma(2, 3));
+
+function initializePage() {
+    console.log("***Oload Completed***");
+    console.log(navigator.userAgent);
+    console.log(navigator.language);
+    console.log(navigator.languages);
+    //tarea, implementar la funcinalidad de copiado
+    //crear un input text con algun texto predifinido -> UN BOTON ""Copiar -> cipie en el clipboard
+    //Pegar el texto en un notepad, en algun elemento visual que permita comprobar que se ha copiado
+    console.log(navigator.onLine);
+}
